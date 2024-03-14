cd3bf78f34a19cf7470488c66e222a13234d693c
Olya <122615170+olyalupatsa@users.noreply.github.com>
2024-03-14 22:35:07 +0200

    1

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,6 @@ import Options from './components/Options/Options';
 import Feedback from './components/Feedback/Feedback';
 import Notification from './components/Notification/Notification';
 const App = () => {
- 
   const initialFeedbackState = JSON.parse(localStorage.getItem('feedback')) || { good: 0, neutral: 0, bad: 0 };
   const [feedback, setFeedback] = useState(initialFeedbackState);
 
@@ -11,26 +10,27 @@ const App = () => {
     localStorage.setItem('feedback', JSON.stringify(feedback));
   }, [feedback]);
 
-  useEffect(() => {
-    const handleBeforeUnload = () => {
-      localStorage.removeItem('feedback');
-    };
+  const handleFeedback = (type) => {
+    setFeedback(prevFeedback => ({
+      ...prevFeedback,
+      [type]: prevFeedback[type] + 1
+    }));
+  };
 
-    window.addEventListener('beforeunload', handleBeforeUnload);
-
-    return () => {
-      window.removeEventListener('beforeunload', handleBeforeUnload);
-    };
-  }, []);
   const resetFeedback = () => {
     setFeedback({ good: 0, neutral: 0, bad: 0 });
   };
+
+
+  const total = feedback.good + feedback.neutral + feedback.bad;
+  const positivePercentage = Math.round((feedback.good / total) * 100);
+
   return (
     <div>
       <h1>Sip Happens Café</h1>
       <p>Please leave your feedback about our service by selecting one of the options below.</p>
-      <Options setFeedback={setFeedback} resetFeedback={resetFeedback} />
-      <Feedback feedback={feedback} />
+      <Options setFeedback={handleFeedback} resetFeedback={resetFeedback} />
+      <Feedback feedback={feedback} positivePercentage={positivePercentage} />
       <Notification feedback={feedback} />
     </div>
   );
